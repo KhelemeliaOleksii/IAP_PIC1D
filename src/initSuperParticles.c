cf279279e0f4e6958e43ff25dc65d32d2a7f9fe9
KhelemeliaOleksii <xvdm@ukr.net>
2023-03-08 15:08:53 +0200

    start test

--- src/initSuperParticles.c
+++ src/initSuperParticles.c
@@ -4,19 +4,19 @@
 #include <stdio.h>
 
 int initSuperParticles () {
-  // pointer to a name of velocity distribution function
-  char * dvf_description[200];
-  // pointer to a name of coord distribution function
-  char * dcf_description[200];
-  // pointer to an array of particles coord
-  double* particles_coord[(int)SUPERPRTCL_NMBR];
-  // pointer to an array of particles velocity
-  double* particles_velocity[(int)SUPERPRTCL_NMBR];
+  // array to a name of velocity distribution function
+  char dvf_description[200];
+  // array to a name of coord distribution function
+  char dcf_description[200];
+  // array to an array of particles coord
+  double particles_coord[(int)SUPERPRTCL_NMBR];
+  // array to an array of particles velocity
+  double particles_velocity[(int)SUPERPRTCL_NMBR];
   
   // Length of plasma
   double length_total = CELLS * LENGTH_MESH; 
 
-  double plasma_borders[0] = {0, length_total};
+  double plasma_borders[2] = {0, length_total};
   // Real particles (rp) all
   double rp_all = DENSITY_FIRST * length_total;
 
@@ -33,11 +33,12 @@ int initSuperParticles () {
   fprintf(stdout, "Real particles per a SuperPArticle are %e \n", rp_per_sp);
   fprintf(stdout, "Charge of a SuperParticle is %e C\n", charge_sp);
   fprintf(stdout, "Mass of a SuperParticle is %e kg\n", mass_sp);
+  fprintf(stdout, "Plasma borders %e<->%e\n", plasma_borders[0], plasma_borders[1]);
   fflush(stdout);
   
 
-  initParticlesPosition(SUPERPRTCL_NMBR, plasma_borders, distribute1DCoordUniform, &particles_coord, &dcf_description);
-  initParticlesVelocity(SUPERPRTCL_NMBR, VELOCITY_FIRST, distribute1DVelocityUniform, &particles_velocity, &dvf_description);
+  //initParticlesPosition(SUPERPRTCL_NMBR, plasma_borders, distribute1DCoordUniform, &particles_coord, &dcf_description);
+  //initParticlesVelocity(SUPERPRTCL_NMBR, VELOCITY_FIRST, distribute1DVelocityUniform, &particles_velocity, &dvf_description);
   return 0;
 }
 
@@ -45,7 +46,7 @@ int initParticlesPosition(int particle_number, double **borders, int distribute_
   double start = borders[0][0];
   double end = borders[0][1];
 
-  distributeCoords(particle_number, start, end, &particle_position, &function_description);
+  distribute_function(particle_number, start, end, &particle_position, &function_description);
 
   return 0;
 } 
